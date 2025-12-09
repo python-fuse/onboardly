4b0ebca33895714c64a6e1e1e793fd12ab75a8d8
Umar Sulaiman <akoredeumar8@gmail.com>
2025-12-09 01:43:34 +0100

    I added a Dashboard Navbar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,11 @@
+import DashboardNavbar from "@/src/components/dashboard/DashboardNavbar";
 import StepsManager from "@/src/components/dashboard/StepsManager";
 import React from "react";
 
 function Dashboard() {
   return (
     <>
+      <DashboardNavbar />
       <StepsManager />
     </>
   );
